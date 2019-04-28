d73b8e004492a6d131a001900a473ca34535a0fd
Katharine Cooney <katharinecooney@gmail.com>
2019-04-28 11:44:59 +0200

    Add spotify credentials, installed express, spotify, hbs, and prettyjson packages

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
+const SpotifyWebApi = require('spotify-web-api-node');
 
 
 const app = express();
@@ -14,9 +12,22 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 
+const clientId = '45918a8777794c6aabb67dd56cd893a4',
+    clientSecret = 'a8d8d3132f6343939119c9933eaffeb8';
 
+const spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
 
-
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then( data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  })
+  .catch(error => {
+    console.log('Something went wrong when retrieving an access token', error);
+  })
 
 
 // the routes go here:
